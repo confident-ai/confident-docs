2556d597a8e80272207246be6e78418f40f97350
penguine-ip <jeffreyip@confident-ai.com>
2025-07-30 16:02:47 -0700

    .

--- content/docs/llm-evaluation/_meta.js
+++ content/docs/llm-evaluation/_meta.js
@@ -1,15 +1,12 @@
 export default {
   introduction: '',
+  quickstart: '',
+  "end-to-end-evals": '',
+  "component-level-evals": '',
   "metrics": {
     title: "Metrics",
     theme: {
       collapsed: true
-    }
-  },
-  "run-evals": {
-    title: "Running Evals",
-    theme: {
-      collapsed: false
     }
   },
   "evaluation-features": {
